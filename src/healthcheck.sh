8feeb9404a3c11ab06ec56bc2db6ba2731927192
Nicholas Keeney <aquaculture@oceanics.io>
2019-06-22 12:16:07 -0700

    condacache

--- src/healthcheck.sh
+++ src/healthcheck.sh
@@ -8,7 +8,7 @@ status="undefined"
 while [ "$status" != *"healthy"* ]
 do
     count=${count}+1
-    if [ count -gt retries ]
+    if [ ${count} -gt ${retries} ]
     then
         echo "Neo4j status is ${status}, aborting"
         docker-compose down
@@ -16,7 +16,7 @@ do
     fi
     echo "Neo4j status is ${status}, waiting 5 seconds"
     sleep ${timeout}
-    status=$(docker inspect --format='{{json .State.Health.Status}}' bathysphere-graph_neo4j_1)
+    status=$(sudo docker inspect --format='{{json .State.Health.Status}}' bathysphere-graph_neo4j_1)
 done
 echo "Neo4j status is ${status}, continuing with tests"
 exit 0
