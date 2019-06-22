1ad0730647eccae7cd7145ef98d50bd269302012
Nicholas Keeney <aquaculture@oceanics.io>
2019-06-22 12:20:47 -0700

    count error

--- src/healthcheck.sh
+++ src/healthcheck.sh
@@ -7,7 +7,6 @@ status="undefined"
 
 while [ "$status" != *"healthy"* ]
 do
-    count=${count}+1
     if [ ${count} -gt ${retries} ]
     then
         echo "Neo4j status is ${status}, aborting"
@@ -15,8 +14,10 @@ do
         exit 1
     fi
     echo "Neo4j status is ${status}, waiting 5 seconds"
+    ((count++))
     sleep ${timeout}
     status=$(sudo docker inspect --format='{{json .State.Health.Status}}' bathysphere-graph_neo4j_1)
 done
 echo "Neo4j status is ${status}, continuing with tests"
 exit 0
+
