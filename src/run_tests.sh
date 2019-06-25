ff9d2522e0c72ee43d782fa3a9cedfc0093460e4
Nicholas Keeney <aquaculture@oceanics.io>
2019-06-25 01:32:31 -0700

    Content Policy Netlify

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker-compose up -d neo4j
-docker run -it --entrypoint="./src/pytest.sh" oceanicsdotio/bathysphere-graph
+docker-compose run -it --entrypoint="./src/pytest.sh" oceanicsdotio/bathysphere-graph
 docker-compose down
