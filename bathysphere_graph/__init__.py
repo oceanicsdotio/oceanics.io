755c36d10a44dbed8cc45ad286d5bffb451f98ec
Nicholas Keeney <aquaculture@oceanics.io>
2019-06-27 10:06:17 -0700

    add build to deploy.sh

--- bathysphere_graph/__init__.py
+++ bathysphere_graph/__init__.py
@@ -12,7 +12,7 @@ try:
 except ImportError:
     defaults = {
         'ADMIN': '',
-        'NEO4J_AUTH': '/',
+        'NEO4J_AUTH': 'neo4j/n0t_passw0rd',
         'ADMIN_PASS': '',
         'API_KEY': '',
         'HOST': 'localhost',
