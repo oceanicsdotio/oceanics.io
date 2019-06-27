ced8aa4fdaed2ffdd75dece86dfa64e90cd23ae6
Nicholas Keeney <aquaculture@oceanics.io>
2019-06-27 10:06:17 -0700

    add build to deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+cd bathysphere-graph
 docker-compose down
 sleep 5
 git pull
 docker-compose build bathysphere-graph
 docker-compose run --entrypoint="./src/pytest.sh" bathysphere-graph
+docker-compose down
 docker-compose up -d
