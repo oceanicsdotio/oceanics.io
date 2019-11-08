1c9f0b92f9a5c55559232a54bdb0278b4a181a43
Nicholas Keeney <aquaculture@oceanics.io>
2019-11-08 09:19:55 -0800

    move FTP methods into project from bathysphere core

--- bathysphere_graph/__init__.py
+++ bathysphere_graph/__init__.py
@@ -3,31 +3,10 @@ from os import getenv
 from flask_cors import CORS
 from yaml import Loader, load as load_yml
 
-appConfig = load_yml(open("config/app.yml"), Loader)
-app = App(__name__, specification_dir="../openapi/", options={"swagger_ui": False})
 
-for key, value in {
-    "ADMIN": "",
-    "NEO4J_AUTH": "neo4j/neo4j",
-    "ADMIN_PASS": "",
-    "API_KEY": "",
-    "HOST": "localhost",
-    "EMAIL_ACCT": None,
-    "EMAIL_KEY": None,
-    "EMAIL_SERVER": None,
-    "EMAIL_PORT": None,
-    "REPLY_TO": None,
-    "SECRET": None,
-    "PORT": 5000,
-    "BASE_PATH": "/api",
-    "DOCKER_COMPOSE_NAME": "bathysphere-graph_neo4j_1",
-    "DOCKER_CONTAINER_NAME": "neo4j",
-    "EMBEDDED_NAME": "localhost",
-    "RETRIES": 3,
-    "DELAY": 10,
-    "NEO4J_PORT": 7687,
-    "NEO4J_HOST": None,
-}.items():
+appConfig = load_yml(open("config/app.yml"), Loader)
+app = App(__name__, specification_dir=appConfig["environment"]["SPEC_PATH"], options={"swagger_ui": False})
+for key, value in appConfig["environment"].items():
     app.app.config[key] = getenv(key, value)
 
 app.add_api("api.yml", base_path=app.app.config["BASE_PATH"])
