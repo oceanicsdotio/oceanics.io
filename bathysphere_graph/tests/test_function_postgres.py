3d3398b821bc28efdf3ddaf26e047bb7298ead59
Nicholas Keeney <aquaculture@oceanics.io>
2019-11-16 01:15:32 -0800

    fcn queue secrets removed

--- bathysphere_graph/tests/test_function_postgres.py
+++ bathysphere_graph/tests/test_function_postgres.py
@@ -1,7 +1,3 @@
-from datetime import datetime
-from random import random
-from collections import OrderedDict
-from time import sleep
 from requests import post
 from json import dumps
 from os import getenv
@@ -9,123 +5,13 @@ import hmac
 import hashlib
 
 
-from bathysphere_graph.models import Locations, Collections, Things
-
-PG_DP_NULL = "DOUBLE PRECISION NULL"
-PG_TS_TYPE = "TIMESTAMP NOT NULL"
-PG_GEO_TYPE = "GEOGRAPHY NOT NULL"
-PG_ID_TYPE = "INT PRIMARY KEY"
-PG_STR_TYPE = "VARCHAR(100) NULL"
-
-HMAC_KEY = getenv("HMAC_KEY", "53cr3t50fth3e53@")
+from bathysphere_graph.models import Collections
 
 
-def test_postgres_openfaas_ingest_series_async():
-    """Submit rows through OpenFaaS"""
-    data = dumps(
-        {
-            "table": "test",
-            "fields": OrderedDict(
-                time=PG_TS_TYPE,
-                temperature=PG_DP_NULL,
-                salinity=PG_DP_NULL,
-                pressure=PG_DP_NULL,
-            ),
-            "data": tuple(
-                (
-                    datetime.now().isoformat()[:-7],
-                    20.0 + random(),
-                    30.0 + random(),
-                    10.0 + random(),
-                )
-                for _ in range(10)
-            ),
-        }
-    )
-    response = post(
-        url="http://faas.oceanics.io:8080/async-function/postgres",
-        data=data,
-        headers={
-            "hmac": hmac.new(HMAC_KEY.encode(), data.encode(), hashlib.sha1).hexdigest()
-        },
-    )
-    assert response.status_code == 202
+HMAC_KEY = getenv("HMAC_KEY")
 
 
-def test_postgres_openfaas_retrieve_series():
-    data = dumps({"table": "test", "encoding": "json", "fields": ["temperature"]})
-    response = post(
-        url="http://faas.oceanics.io:8080/function/postgres",
-        data=data,
-        headers={
-            "hmac": hmac.new(HMAC_KEY.encode(), data.encode(), hashlib.sha1).hexdigest()
-        },
-        stream=False,
-    )
-    assert response.ok, response.json()
-
-
-def test_postgres_openfaas_ingest_postgis_polygons_async():
-
-    data = dumps(
-        {
-            "table": "test_locations",
-            "fields": OrderedDict(id=PG_ID_TYPE, name=PG_STR_TYPE, geo=PG_GEO_TYPE),
-            "data": [
-                [
-                    0,
-                    f"location-{0}",
-                    {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [
-                                [0, 45],
-                                [45 + random(), 45 + random()],
-                                [45 + random(), 0 + random()],
-                                [0 + random(), 0 + random()],
-                                [0, 45],
-                            ]
-                        ],
-                        "crs": {"type": "name", "properties": {"name": "EPSG:4326"}},
-                    },
-                ]
-            ],
-        }
-    )
-    response = post(
-        url="http://faas.oceanics.io:8080/async-function/postgres",
-        data=data,
-        headers={
-            "hmac": hmac.new(
-                "53cr3t50fth3e53@".encode(), data.encode(), hashlib.sha1
-            ).hexdigest()
-        },
-    )
-    assert response.status_code == 202
-
-
-def test_postgres_openfaas_retrieve_polygons():
-    data = dumps(
-        {
-            "table": "maine_boundaries_town_polygon",
-            "fields": ["globalid", "town", "county", "shapestare"],
-            "conditions": ["land='n'", "type='coast'"],
-            "encoding": "json",
-        }
-    )
-    response = post(
-        url="http://faas.oceanics.io:8080/function/postgres",
-        data=data,
-        headers={
-            "hmac": hmac.new(
-                "53cr3t50fth3e53@".encode(), data.encode(), hashlib.sha1
-            ).hexdigest()
-        },
-    )
-    assert response.ok, response.content.decode()
-
-
-def test_postgres_create_maine_town_boundaries(psql, create_entity):
+def test_postgres_create_maine_town_boundaries(create_entity):
 
     data = dumps(
         {
---
+++ bathysphere_graph/tests/test_function_queue.py
@@ -0,0 +1,70 @@
+from time import sleep
+from redis import StrictRedis
+from pickle import dumps as pickle, loads as unpickle
+from rq import Queue, Connection, Worker
+
+from tests import jobs
+from bathysphere_graph import app
+
+
+def test_redis_connection():
+
+    cache = StrictRedis(
+        host=app.app.config["REDIS_HOST"],
+        port=app.app.config["REDIS_PORT"],
+        db=0,
+        password=app.app.config["REDIS_KEY"],
+        socket_timeout=3,
+        ssl=True,
+    )
+
+    assert cache.time()
+    key = "test-obj"
+    json = {"name": "something", "description": "test object"}
+    data = pickle(json)
+    assert cache.set(name=key, value=data, ex=10)
+    assert cache.incr("test-hits") >= 1
+    assert unpickle(cache.get(name=key)) == json
+    assert cache.delete(key)
+    assert cache.delete("test-hits")
+
+
+def scheduleJob():
+
+    cache = StrictRedis(
+        host=app.app.config["REDIS_HOST"],
+        port=app.app.config["REDIS_PPORT"],
+        db=0,
+        password=app.app.config["REDIS_KEY"],
+        socket_timeout=None,
+        ssl=True,
+    )
+    q = Queue(connection=cache)  # no args implies the default queue
+    job = q.enqueue(
+        jobs.numberOfTheBeast, args=("foo", "bar"), result_ttl=500, job_timeout=180
+    )
+    print(job.result)  # => None
+
+    # Now, wait a while, until the worker is finished
+    sleep(2)
+    print(job.result)
+
+
+# Provide queue names to listen to as arguments to this script,
+# similar to rq worker
+def redisWorker():
+    cache = StrictRedis(
+        host=app.app.config["REDIS_HOST"],
+        port=app.app.config["REDIS_PORT"],
+        db=0,
+        password=app.app.config["REDIS_KEY"],
+        socket_timeout=None,
+        ssl=True,
+    )
+    with Connection(connection=cache):
+        qs = ["default"]
+        w = Worker(qs)
+        w.work()
+
+
+# redisWorker()
