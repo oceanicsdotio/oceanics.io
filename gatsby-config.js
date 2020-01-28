cf62a1824b652e939d574aac795a6c49b14980a3
Nicholas Keeney <nkeeney@oceanics.io>
2020-01-28 15:59:20 -0800

    remove null ref for icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: null,
       },
     },
     `gatsby-plugin-react-helmet`,
