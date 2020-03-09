7514b7f9f5954a4e925e5cb42b0e958afbf4e7bb
Nicholas Keeney <nkeeney@oceanics.io>
2020-03-08 21:20:35 -0700

    simplify for testin PG functions, after the secrets have been set up on console

---
+++ bathysphere_functions_postgres/main.py
@@ -0,0 +1,67 @@
+from json import dumps
+from os import getenv
+from flask import Flask, Request
+import sqlalchemy
+from google.cloud import secretmanager
+
+PG_DP_NULL = "DOUBLE PRECISION NULL"
+PG_TS_TYPE = "TIMESTAMP NOT NULL"
+PG_GEO_TYPE = "GEOGRAPHY NOT NULL"
+PG_ID_TYPE = "INT PRIMARY KEY"
+PG_STR_TYPE = "VARCHAR(100) NULL"
+
+client = secretmanager.SecretManagerServiceClient()
+project_id = getenv("GCP_PROJECT")
+
+
+def googleCloudSecret(secret_name="my-secret"):
+    # type: (str) -> str
+    resource_name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+    response = client.access_secret_version(resource_name)
+    return response.payload.data.decode('UTF-8')
+
+
+user = googleCloudSecret("pg-username")
+password = googleCloudSecret("pg-password")
+cloudSQL = googleCloudSecret("cloud-sql-connection")
+
+db = sqlalchemy.create_engine(
+
+    sqlalchemy.engine.url.URL(
+        drivername='postgres+pg8000',
+        username=user,
+        password=password,
+        database="bathysphere",
+        query={'unix_sock': f'/cloudsql/{cloudSQL}/.s.PGSQL.5432'}
+    ),
+    pool_size=4,
+    max_overflow=2,
+    pool_timeout=5,
+    pool_recycle=1800,
+)
+
+
+def main(request: Request):
+    """
+    Do some postgres stuff
+    """
+    _hash = getenv("Http_Hmac")
+
+    with db.connect() as conn:
+
+        stmt = sqlalchemy.text(
+            "SELECT text FROM messages;"
+        )
+
+        records = [{
+            'message': row[0]
+        } for row in conn.execute(stmt).fetchall()]
+
+    return dumps({
+        "count": len(records),
+        "data": records,
+        "method": request.method,
+        "hash": _hash,
+        "query_string": request.query_string
+    }), 200
+
