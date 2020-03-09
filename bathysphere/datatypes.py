e2d7f3216dd65d89f9f548a669f4c220ff6148e5
Nicholas Keeney <nkeeney@oceanics.io>
2020-03-09 16:59:23 -0700

    remove ref to Root, explicit imports in graph handlers

---
+++ bathysphere/datatypes.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+ExtentType = (float, float, float, float)
+ResponseJSON = (dict, int)
+ResponseOctet = (dict, int)
+
+
+
+class CoordinateSystem(Enum):
+    Sigma = 1
+    Cartesian = 2
+    Gaussian = 3
+    Spherical = 4
+    Periodic = 5
