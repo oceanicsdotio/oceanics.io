cca93caa35a6ad1ada01c8de414fe2d22e9ccb8f
Nicholas Keeney <nkeeney@oceanics.io>
2020-03-09 19:23:02 -0700

    Readme

---
+++ src/pages/demo.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+import init, {panic_hook} from "../demo/space";
+import {DataStreamLoop} from "../demo/stream.js";
+import {RenderingContext} from '../demo/cadet.js';
+
+
+const DefaultProps = {
+    eid: "data-stream",
+    context: "2d",
+    caption: "DataStream",
+    font: "12px Arial"
+};
+
+
+export default class extends React.Component {
+
+render(props) {
+    const { data } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+    const {eid, autoplay} = props;
+        let paused = (autoplay === undefined) || (!autoplay);
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title={"Situational awareness for a changing ocean"} />
+        <div className={"container"} onClick={async (event) => {
+            if (!paused) {
+                await init();
+                panic_hook();
+                RenderingContext(DataStreamLoop, props);
+            }
+            paused = !paused;
+        }}>
+            <canvas id={eid} width={200} height={200} ></canvas>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+
+    
