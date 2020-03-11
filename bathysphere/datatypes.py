a6233ed02875b0c287597901b52077bb24844719
Nicholas Keeney <nkeeney@oceanics.io>
2020-03-11 16:59:08 -0700

    datatypes up to date

--- bathysphere/datatypes.py
+++ bathysphere/datatypes.py
@@ -22,8 +22,10 @@ from minio.error import NoSuchKey
 from requests import get, post
 from requests.exceptions import ConnectionError
 from urllib3.exceptions import MaxRetryError
+from flask import Response
+from redis import StrictRedis
 
-
+from bathysphere.utils import join, parsePostgresValueIn
 
 try:
     from numpy import array, append
@@ -1007,6 +1009,137 @@ class ObjectStorage(Minio):
             return wrapper
 
         return decorator
+
+        
+    @staticmethod
+    def listenToEvents(storage, bucket_name, filetype="", channel="bathysphere-events"):
+        fcns = ("s3:ObjectCreated:*", "s3:ObjectRemoved:*", "s3:ObjectAccessed:*")
+        r = StrictRedis()
+        ps = r.pubsub()
+        for event in storage.listen_bucket_notification(
+                bucket_name, "", filetype, fcns
+        ):
+            ps.publish(channel, str(event))
+
+    def unlock(self, session, bucket_name, object_name):
+        # type: (Minio, str, str) -> bool
+        """
+        Unlock the dataset or bathysphere_functions_repository IFF it contains the session ID
+        """
+        try:
+            _ = self.stat_object(bucket_name, object_name)
+        except NoSuchKey:
+            return False
+        self.remove_object(bucket_name, object_name)
+        return True
+
+    @staticmethod
+    def locking(self, fcn):
+        async def wrapper(bucket_name, name, sess, headers, *args, **kwargs):
+            # type: (Minio, str, str, str, dict, list, dict) -> Any
+
+            _lock = {"session": sess, "object_name": f"{name}/lock.json"}
+            ObjectStorage.create(
+                bucket_name=bucket_name,
+                object_name=f"{name}/lock.json",
+                buffer=dumps({sess: []}).encode(),
+                metadata={
+                    "x-amz-meta-created": datetime.utcnow().isoformat(),
+                    "x-amz-acl": "private",
+                    "x-amz-meta-service-file-type": "lock",
+                    **(headers if headers else {}),
+                },
+                storage=self,
+                content_type="application/json"
+            )
+            result = fcn(storage=self, dataset=None, *args, session=sess, **kwargs)
+            self.unlock(**_lock)
+            return result
+        return wrapper
+
+    @staticmethod
+    def updateJson(storage, bucket_name, object_name, metadata, entries, props):
+        # type: (Minio, str, str, dict, dict, dict) -> None
+        """
+        Update contents of index metadata
+        """
+        stat = storage.stat_object(bucket_name, object_name)
+        if not entries:
+            storage.copy_object(
+                bucket_name=bucket_name,
+                object_name=object_name,
+                object_source=object_name,
+                metadata=metadata,
+            )
+        else:
+            ObjectStorage.create(
+                storage=storage,
+                buffer=dumps({
+                    **loads(storage.get_object(bucket_name, object_name).data),
+                    **(entries or {}),
+                    **(props or {}),
+                }).encode(),
+                bucket_name=bucket_name,
+                object_name=object_name,
+                metadata={**stat.metadata, **(metadata or {})},
+                content_type="application/json"
+            )
+
+    @staticmethod
+    def streamObject(storage, bucket_name, object_name):
+        # type: (Minio, str, str) -> Response
+        """
+        Retrieve metadata for single item, and optionally the full dataset
+        """
+        obj = storage.get_object(bucket_name, object_name)
+
+        def generate():
+            for d in obj.stream(32 * 1024):
+                yield d
+        return Response(generate(), mimetype="application/octet-stream")
+
+
+    @staticmethod
+    def create(storage, bucket_name, object_name, buffer, content_type, metadata):
+        storage.put_object(
+            bucket_name=bucket_name,
+            object_name=object_name,
+            metadata=metadata,
+            data=BytesIO(buffer),
+            length=len(buffer),
+            content_type=content_type,
+        )
+
+
+    # @staticmethod
+    # def delete(storage, bucket_name, prefix, batch=10):
+    #     """
+    #     Delete all objects within a subdirectory or abstract collection
+
+    #     :param bucket_name: file prefix/dataset
+    #     :param prefix: most to process at once
+    #     :param batch:  number to delete at a time
+    #     """
+    #     remove = ()
+    #     conditions = {"x-amz-meta-service": "bathysphere"}
+
+    #     objects_iter = storage.list_objects(bucket_name, prefix=prefix)
+    #     stop = False
+    #     while not stop:
+    #         try:
+    #             object_name = next(objects_iter).object_name
+    #         except StopIteration:
+    #             stop = True
+    #         else:
+    #             stat = storage.stat_object(bucket_name, object_name).metadata
+    #             if all(stat.get(k) == v for k, v in conditions.items()):
+    #                 remove += (object_name,)
+    #         if len(remove) >= batch or stop:
+    #             storage.remove_objects(bucket_name=bucket_name, objects_iter=remove)
+    #             remove = ()
+    #         if stop:
+    #             break
+
 
 
 class PostgresType(Enum):
