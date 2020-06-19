d7ba761239a794a4cc393f1aeb68b02f5bbdb502
Nicholas Keeney <nkeeney@oceanics.io>
2020-06-19 07:55:07 -0400

    recompile rust

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
-const path = require(`path`)
-const _ = require("lodash")
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
+const _ = require("lodash");
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     if (stage === 'build-html') {
@@ -10,10 +11,60 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
             {
               test: /mapbox-gl/,
               use: loaders.null(),
-            },
-          ],
-        },
+            }
+          ]
+        }
       })
+    } else if (stage === 'develop') {
+      actions.setWebpackConfig({
+        devtool: 'eval-source-map',
+        stats: 'verbose',
+        plugins: [
+          new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, "crate"),
+      
+            // Check https://rustwasm.github.io/wasm-pack/book/commands/build.html for
+            // the available set of arguments.
+            //
+            // Optional space delimited arguments to appear before the wasm-pack
+            // command. Default arguments are `--verbose`.
+            args: "--log-level warn",
+            // Default arguments are `--typescript --target browser --mode normal`.
+            extraArgs: "--no-typescript",
+      
+            // Optional array of absolute paths to directories, changes to which
+            // will trigger the build.
+            // watchDirectories: [
+            //   path.resolve(__dirname, "another-crate/src")
+            // ],
+      
+            // The same as the `--out-dir` option for `wasm-pack`
+            // outDir: "pkg",
+      
+            // The same as the `--out-name` option for `wasm-pack`
+            // outName: "index",
+      
+            // If defined, `forceWatch` will force activate/deactivate watch mode for
+            // `.rs` files.
+            //
+            // The default (not set) aligns watch mode for `.rs` files to Webpack's
+            // watch mode.
+            // forceWatch: true,
+      
+            // If defined, `forceMode` will force the compilation mode for `wasm-pack`
+            //
+            // Possible values are `development` and `production`.
+            //
+            // the mode `development` makes `wasm-pack` build in `debug` mode.
+            // the mode `production` makes `wasm-pack` build in `release` mode.
+            // forceMode: "development",
+      
+            // Controls plugin output verbosity, either 'info' or 'error'.
+            // Defaults to 'info'.
+            // pluginLogLevel: 'info'
+          }),
+        ]
+      });
     }
   }
 
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+// import { DataStream, draw_fps, draw_caption } from "../utils/space";
+
+
+const GLSL_DIRECTORY = "../../glsl-src";
 
 const StyledCanvas = styled.canvas`
     border-radius: 3px;
@@ -8,12 +12,89 @@ const StyledCanvas = styled.canvas`
     width: 100%;
 `;
 
-export default () => {
+export default (props) => {
 
-    let radius = 0;
-    const [state, setState] = useState(null);
+    const canvasRef = React.useRef(null)
+    
+    const renderProps = {
+        eid: "data-stream", 
+        context: "2d", 
+        caption: "DataStream", 
+        font: "12px Arial",
+        canvas: {
+            height: 200,
+            width: 200
+        }
+    }
+
+    useEffect(() => {
+
+        async function run () {
+            props = renderProps;
+            const wasm = await import('../utils/space');
+            console.log(wasm);
+            if (!props.canvas) throw Error(`No canvas element "${props.eid}".`);
+
+            const ctx = props.ctx = canvasRef.current.getContext(props.context);
+            if (!ctx) throw Error(`No "${props.context}" rendering context for ${props.eid}.`);
+
+            if (props.context === "webgl" && typeof props.shaders == "undefined") {
+                throw Error(`Shaders required to start "${props.context}" rendering context.`);
+            } else if (props.context === "webgl") {
+                let source = null;
+                try {
+                    source = Object.fromEntries(await Promise.all([...new Set(Object.values(props.shaders).flat())]
+                        .map(async key => [key, await wasm.fetch_text(`${GLSL_DIRECTORY}/${key}.glsl`)])));
+                } catch (e) {
+                    throw Error(`Could not load shaders from ./${GLSL_DIRECTORY}`);
+                }
+
+                props.programs = Object.keys(props.shaders).reduce((obj, key) => {
+                    let [vs, fs] = props.shaders[key];
+                    const program = wasm.create_program(ctx, source[vs], source[fs]);
+                    let wrapper = {program: program};
+                    for (let i = 0; i < ctx.getProgramParameter(program, ctx.ACTIVE_ATTRIBUTES); i++) {
+                        let attribute = ctx.getActiveAttrib(program, i);
+                        wrapper[attribute.name] = ctx.getAttribLocation(program, attribute.name);
+                    }
+                    for (let i = 0; i < ctx.getProgramParameter(program, ctx.ACTIVE_UNIFORMS); i++) {
+                        let uniform = ctx.getActiveUniform(program, i);
+                        wrapper[uniform.name] = ctx.getUniformLocation(program, uniform.name);
+                    }
+                    obj[key] = wrapper;
+                    return obj;
+                }, {});
+            }
+
+            props.start = performance.now() | 0.0;
+
+            let {canvas, caption, font} = props;
+            let {height} = canvas;
+            let frames = 0;
+            let stream = new wasm.DataStream(200);
+        
+            const _render = () => {
+                const time = (performance.now() | 0.0 )- props.start;
+                stream.push(time, Math.random());
+                stream.draw(ctx, 200.0, 200.0, "#FFFFFF");
+                frames = wasm.draw_fps(ctx, frames, time, "#FFFFFF");
+                wasm.draw_caption(ctx, caption, 0.0, height, "#CCCCCC", font);
+                requestAnimationFrame(_render);
+            };
+            _render();
+            
+            
+        }
+        run();
+     }, [])
 
     return (
-        <StyledCanvas></StyledCanvas>
+    
+        <StyledCanvas 
+            id={renderProps.eid}
+            ref={canvasRef}
+            height={200}
+            width={200}
+        />
     );
 };
