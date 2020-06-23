3d4d513201616c24c4d7f3d64b11c4c243ca3f18
Nicholas Keeney <nkeeney@oceanics.io>
2020-06-23 07:42:25 -0400

    object storage parsing and struct

--- src/components/Storage.js
+++ src/components/Storage.js
@@ -4,26 +4,49 @@ export default () => {
 
     const nullState = {};
     const [ summary, setSummary ] = useState(nullState);
-    const [ fileSystem, setFileSystem ] = useState("");
+    const [ fileSystem, setFileSystem ] = useState(nullState);
+
 
     useEffect(() => {
         
         (async () => {
             fetch(
-                "https://oceanicsdotio.nyc3.digitaloceanspaces.com/",
+                "https://oceanicsdotio.nyc3.digitaloceanspaces.com",
                 {
                     method: 'GET',
                     mode: 'cors',
-                    cache: 'no-cache',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
+                    cache: 'no-cache'
                 }
             )
                 .then(response => response.text())
                 .then(text => {
-                    console.log(text);
-                    setFileSystem(text);
+                    const parser = new DOMParser();
+                    const xmlDoc = parser.parseFromString(text,"text/xml");
+
+                    let nodes = {};
+                    
+                    xmlDoc.childNodes[0].childNodes.forEach(node => {
+                        if (node.tagName == "Contents") {
+
+                            const key = node.childNodes[0].textContent;
+                            const path = key.split("/");
+                            const _node = {
+                                key: key,
+                                updated: node.childNodes[1].textContent,
+                                size: node.childNodes[3].textContent,
+                            }
+
+                            if (path[0] in nodes) {
+                                nodes[path[0]].push(_node);
+                            } else {
+                                nodes[path[0]] = [_node];
+                            }
+                            
+                        }
+                        
+                        
+                    });
+                    setFileSystem(nodes);
                 })
                 .catch(err => console.log(err))
         })()
@@ -32,9 +55,18 @@ export default () => {
     return (
         <div>
             <h2>Object storage interface</h2>
-            <p>
-                Some statistics go here: {fileSystem}
-            </p>
+            <ol>
+            {Object.entries(fileSystem).map(([collection, nodes]) => {
+                return (
+                    <li key={collection}>
+                        <ul>
+                            {nodes.map(node => <li>{node.key} ({(parseFloat(node.size) / 1000.0).toFixed(1)} kb)</li>)}
+                        </ul>
+                    </li>
+                );
+            })}
+            </ol>
+            
         </div>
     );
 };
