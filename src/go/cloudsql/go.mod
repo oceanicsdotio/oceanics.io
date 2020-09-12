26857786d489dbc3eb35a791e6e07defa21d0235
Nicholas Keeney <nkeeney@oceanics.io>
2020-09-12 12:20:42 -0400

    pick up ENV in Go fcn

--- src/go/cloudsql/go.mod
+++ src/go/cloudsql/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/aws/aws-lambda-go/events
 	github.com/aws/aws-lambda-go/lambda
+    github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres
 )
--- src/go/cloudsql/main.go
+++ src/go/cloudsql/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
+
 )
 
+// Handler gets some Postgres data
 func Handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+	dsn := fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable",
+                   os.Getenv("INSTANCE_CONNECTION_NAME"),
+                   os.Getenv("DATABASE_NAME"),
+                   os.Getenv("DATABASE_USER"),
+				   os.Getenv("PASSWORD"))
+				   
+	db, err := sql.Open("cloudsqlpostgres", dsn)
+
 	// name := request.QueryStringParameters["name"]
 	response := fmt.Sprintf("Hello %s!", "World")
 
