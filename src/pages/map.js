01306b975fcf4ac8372f0a7a46a9fc32c34140bd
Nicholas Keeney <nkeeney@oceanics.io>
2020-10-11 12:41:01 -0400

    replace style json with yaml

--- src/pages/map.js
+++ src/pages/map.js
@@ -2,26 +2,25 @@
 import React from "react"
 import { graphql } from "gatsby";
 
-import YAML from "yaml";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import Map from "../components/Map";
-import style from "../../static/style.json";
+import style from "../../static/style.yml";
 import layers from "../../static/layers.yml";
 
 export default ({
     data: {
         site: {
-            siteMetadata: {title}
+            siteMetadata: { title }
         }
-    }, 
+    },
     location,
 }) => {
     return (
-      <Layout location={location} title={title}>
-        <SEO title="Ocean analytics as a service" /> 
-        <Map style={style} layers={layers}/>
-      </Layout>
+        <Layout location={location} title={title}>
+            <SEO title="Ocean analytics as a service" />
+            <Map style={style} layers={layers} />
+        </Layout>
     )
 };
 
