dcdc35902b30f23a6f2780696304600449b2052e
Nicholas Keeney <nkeeney@oceanics.io>
2020-11-03 11:18:54 -0500

    typo in actions

---
+++ config/gcp-teardown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PROJECT=my-google-project-id
+if [ -z $1 ] ; then
+   echo "Missing argument"
+   exit 1
+fi
+echo "Deleting instance and firewall rules"
+gcloud compute instances delete --quiet "$1" --project "$PROJECT" && gcloud compute firewall-rules --quiet delete "$1" --project "$PROJECT"
+exit $?
