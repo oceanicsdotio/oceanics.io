df19888672c704801e8c41230e0090f11beb9996
Nicholas Keeney <nkeeney@oceanics.io>
2020-12-22 06:14:46 -0500

    make lev distance more JS-y

--- functions/lexicon/lexicon.js
+++ functions/lexicon/lexicon.js
@@ -4,57 +4,55 @@ const words = readFileSync("/usr/share/dict/words").toString().split("\n");
 const word1 = "ocean";
 const word2 = "Aegean";
 
-
-const levenshteinDistance = (word1, word2) => {
-        
-    const columns = word1.length + 1;
-    const rows = word2.length + 1; 
-
+/**
+ * Calculate the similarity of two patterns, usually words
+ * for the purpose of auto-correct or spell checking
+ */
+const levenshteinDistance = (a, b) => {
+         
     // build first row
-    let currentRow = [...Array(columns).keys()];
+    let row = [...Array(a.length + 1).keys()];
    
-    for (let row = 1; row < rows; row++) {
-        let previousRow = currentRow;
-        currentRow = [previousRow[0] + 1];
+    // iterate rows
+    for (let ii = 1; ii < b.length + 1; ii++) {
 
-        for (let column = 1; column < columns; column++) {
-            
-            const insertCost = currentRow[column - 1] + 1;
-            const deleteCost = previousRow[column] + 1;
+        const previous = row;
+        row = [previous[0] + 1];
 
-            let replaceCost;
-            if (word1[column - 1] !== word2[row - 1]) {
-                replaceCost = previousRow[column - 1] + 1;
-            } else {
-                replaceCost = previousRow[column - 1];
-            }
-            currentRow.push(Math.min(insertCost, deleteCost, replaceCost));
+        // iterate columns
+        for (let jj = 1; jj < a.length + 1; jj++) {
+            row.push(Math.min(
+                row[jj - 1] + 1, // insert, 
+                previous[jj] + 1, // delete, 
+                previous[jj - 1] + (a[jj - 1] !== b[ii - 1] | 0) // replace
+            ));
         }
     }
 
-    return currentRow.pop();
+    return row.pop();
 };
 
 
-const search = ({words, pattern, maxCost}) => {
-    console.log({pattern, maxCost});
+
+/**
+ * Recursive descend through a Trie object.
+ * 
+ * If the previous row is not supplied, assume that it is the entry point
+ * and assigned the default first row.
+ * 
+ * @param {*} param0 
+ */
+function searchRecursive({node, symbol, pattern, previous=null, cost}) {
+
+    if (!previous) previous = [...Array(word1.length + 1).keys()]
     
-    return words.reduce((acc, word) => { 
-        const cost = levenshteinDistance(pattern, word);
-        if (cost <= maxCost) acc.push([word, cost]);
-        return acc;
-      }, []);
-};
-
-function searchRecursive({node, symbol, pattern, previous, cost}) {
-
     let row = [previous[0] + 1];
 
-    for (let column = 1; column < pattern.length + 1; column++) {
+    for (let jj = 1; jj < pattern.length + 1; jj++) {
         row.push(Math.min(
-            row[column - 1] + 1,
-            previous[column] + 1,
-            previous[column - 1] + parseInt(pattern[column - 1] !== symbol),
+            row[jj - 1] + 1,
+            previous[jj] + 1,
+            previous[jj - 1] + parseInt(pattern[jj - 1] !== symbol),
         ));
     }
     
@@ -82,8 +80,32 @@ const Node = () => Object({
 });
 
 
-const insert = ({node, key}) => {
-    [...key].forEach(c => {
+/**
+ * Simple iterative search loops through all words and preserves
+ * a record of those which satisfy the maximum mutations
+ * 
+ * @param {*} param0 
+ */
+const search = ({words, pattern, maxCost}) => {
+    console.log({pattern, maxCost});
+    
+    return words.reduce((acc, word) => { 
+        const cost = levenshteinDistance(pattern, word);
+        if (cost <= maxCost) acc.push([word, cost]);
+        return acc;
+      }, []);
+};
+
+/**
+ * Insert a pattern into a Trie-like data structure.
+ * 
+ * In this case, we assume the struct is an object, containing
+ * self-similar nested objects.
+ * 
+ * @param {*} param0 
+ */
+const insert = ({node, pattern}) => {
+    [...pattern].forEach(c => {
         if (!(c in node.children)) {
             node.children[c] = Node();
         }
@@ -110,17 +132,16 @@ const insert = ({node, key}) => {
 // }
 
 const node = Node();
-words.forEach(key => insert({node, key}));
+words.forEach(pattern => insert({node, pattern}));
 
 console.log({
     word1, 
     search: search({words, pattern: word1, maxCost: 2}),
-    node,
-    recursive: Object.entries(node.children).map(([k,v])=>searchRecursive({
-        node: v, 
-        pattern: word1, 
-        cost: 2,
-        previous: [...Array(word1.length + 1).keys()],
-        symbol: k
-    }))
+    // node,
+    // recursive: Object.entries(node.children).map(([k,v])=>searchRecursive({
+    //     node: v, 
+    //     pattern: word1, 
+    //     cost: 2,
+    //     symbol: k
+    // }))
 });
