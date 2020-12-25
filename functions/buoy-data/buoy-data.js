9c5b1a3a06354112ce09ac2de4b41a9bbf09c937
Nicholas Keeney <nkeeney@oceanics.io>
2020-12-25 09:47:39 -0500

    buoy data in JS

--- functions/buoy-data/buoy-data.js
+++ functions/buoy-data/buoy-data.js
@@ -1,54 +1,50 @@
 
-// def report_buoy_data(body):
-// """We receive the hashed message in form of a header"""
-// from collections import deque
-// from requests import get
-// from os import getenv
-// from json import dumps
-// from itertools import repeat
 
-// interval = body.get("interval", (None, None))
-// limit = body.get("limit", None)
-// encoding = body.get("encoding", "txt")
-// node = body.get("id", None)
-// fields = body.get("observedProperties", None)
+exports.handler = async ({
+    queryParameters: {
+        interval=[null, null], 
+        limit=null, 
+        encoding="text", 
+        node=null,
+        observedProperties=null
+}}) => {
 
-// if (
-//     not any((limit, *interval))
-//     or not any((fields, node))
-//     or encoding not in ("txt", "json")
-// ):
-//     return dumps({"Error": "Bad Request"}), 400
+    const badRequest = 
+        (!limit && !interval[0] && !interval[1]) || 
+        (!observedProperties && !node) || 
+        (!(encoding in ["text", "json"]));
 
-// host = getenv("hostname", "maine.loboviz.com")
-// times = (
-//     f"&newest={limit}" if limit else "&min_date={}&max_date={}".format(*interval)
-// )
-// url = f"http://{host}/cgi-data/nph-data.cgi?data_format=text&node={node}&y={','.join(fields)}{times}"
-// response = get(url)
-// content = response.content.decode()
-// if not response.ok:
-//     return response
+    if (badRequest) return {body: "Bad request", statusCode: 400};
 
-// if encoding == "txt":
-//     return content, 200
+    const times = limit ? `&newest=${limit}` : "&min_date={}&max_date={}".format(...interval);
+    let content;
 
-// lines = deque(filter(lambda x: len(x), content.split("\n")))
-// name, alias = lines.popleft().split("-")
-// data = {
-//     "name": name,
-//     "aliases": list(set(map(str.strip, (alias, lines.popleft())))),
-// }
-// lines = deque(map(lambda x: tuple(x.split("\t")), lines))
-// keys = lines.popleft()
-// return (
-//     dumps(
-//         {
-//             **data,
-//             "values": [
-//                 dict(zip(k, v)) for k, v in zip(repeat(keys, len(lines)), lines)
-//             ],
-//         }
-//     ),
-//     200,
-// )
+    fetch(
+        `http://${process.env.BUOY_DATA_HOSTNAME}/cgi-data/nph-data.cgi?data_format=text&node=${node}&y=${fields.join(",")}${times}`
+    ).then(
+        response => response.text()
+    ).then(
+        text => {
+            if (encoding === "text"){
+                content=text;
+            } else if (encoding === "json") {
+                let lines = text.split("\n").filter(x=>x.length|false);
+                const [name, alias] = lines.shift().split("-");
+                const aliases = [...(new Set([alias, lines.shift()].map(x=>x.trim())))];
+                
+                lines = lines.map(x=>x.split("\t"));
+                const keys = lines.shift();
+                content = JSON.stringify({
+                    name,
+                    aliases,
+                    values: lines.map(line => Object.fromEntries(line.map((field, ii)=>[keys[ii], field])))
+                });
+            }
+        }
+    );
+    
+  
+    return {body: content, statusCode: 200};
+           
+};
+
