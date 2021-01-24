3816f9afd30fdd06fabe8e4e4dfeb2f30e140ba8
Nicholas Keeney <nkeeney@oceanics.io>
2021-01-24 12:35:57 -0500

    shaders

--- src/hooks/useGlslShaders.jsx
+++ src/hooks/useGlslShaders.jsx
@@ -9,6 +9,36 @@ import screenFragment from "raw-loader!../glsl/screen-fragment.glsl";
 import drawVertex from "raw-loader!../glsl/draw-vertex-test.glsl";
 import drawFragment from "raw-loader!../glsl/draw-fragment-test.glsl";
 
+/**
+ * Abstraction for binding array data into GPU memory
+ */
+export class ArrayBuffer {
+    constructor(ctx, data) {
+        this.buffer = ctx.createBuffer();
+        ctx.bindBuffer(ctx.ARRAY_BUFFER, this.buffer);
+        ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(data), ctx.STATIC_DRAW);
+    }
+};
+
+/**
+ * Name of the GLSL variable for particle positions
+ */
+const VERTEX_ARRAY_BUFFER = "a_index";
+
+/**
+ * Name of the GLSL variable for rectangle of two triangles
+ */
+const QUAD_ARRAY_BUFFER = "a_pos";
+
+
+/**
+ * Generate the array buffers and handles for double buffering.
+ */
+export const VertexArrayBuffers = (ctx, vertexArray) => Object({
+    quad: [(new ArrayBuffer(ctx, [0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1])).buffer, QUAD_ARRAY_BUFFER, 2],
+    index: [(new ArrayBuffer(ctx, vertexArray)).buffer, VERTEX_ARRAY_BUFFER, 1] 
+});
+
 
 // memoize the shaders as they are loaded
 const shaderSource = {
@@ -20,112 +50,14 @@ const shaderSource = {
     "draw-fragment": drawFragment,
     "draw-vertex": drawVertex
 };
-/**
-IFF the canvas context is WebGL and we need shaders, fetch the GLSL code and compile the 
-shaders as programs.
-
-This is executed only once, after the WASM runtime is loaded. 
-*/
-export const useGlslShaders = ({
-    ref, 
-    shaders,
-    callback=null
-}) => {
-   
-    const [programs, setPrograms] = useState(null);
-    const runtime = useWasmRuntime();
-
-    useEffect(() => {
-
-        if (!runtime || typeof ref === "undefined" || !ref.current) return;
-        const ctx = ref.current.getContext("webgl");
-        if (!ctx) return;
-
-        (async () => {
-            
-            const compiled = Object.fromEntries(await Promise.all(Object.entries(shaders).map(async ([programName, pair]) => {
-    
-                let [vs, fs] = pair.map(async (file) => {
-                    if (!(file in shaderSource)) shaderSource[file] = await runtime.fetch_text(`/${file}.glsl`)
-                    return shaderSource[file];
-                });
-                const program = runtime.create_program(ctx, await vs, await fs);
-                let wrapper = { program };
-
-                [
-                    ["ATTRIBUTES", "Attrib"], 
-                    ["UNIFORMS", "Uniform"]
-                ].forEach(
-                    ([key, fcn])=>{
-                        for (let ii = 0; ii < ctx.getProgramParameter(program, ctx[`ACTIVE_${key}`]); ii++) {
-                            const { name } = ctx[`getActive${fcn}`](program, ii);
-                            wrapper[name] = ctx[`get${fcn}Location`](program, name);
-                        }
-                    }
-                );
-
-                return [programName, wrapper];
-            })));
-    
-            setPrograms(compiled);
-        })();
-    }, [runtime, ref]);
-
-    return {programs}
-
-};
-
-export default useGlslShaders;
-
-
-export const extractUniforms = (keys, uniforms) => 
-    keys.map(k => [k, uniforms[k]]);
-
-
-
-export const createTexture = ({ 
-    ctx, 
-    filter = "NEAREST", 
-    data, 
-    shape = [null, null] 
-}) => {
-
-    let texture = ctx.createTexture();
-    const args = data instanceof Uint8Array ? [...shape, 0] : [];
-
-    ctx.bindTexture(ctx.TEXTURE_2D, texture);
-    const textureArgs = [ctx.TEXTURE_2D, 0, ctx.RGBA, ...args, ctx.RGBA, ctx.UNSIGNED_BYTE, data];
-
-    try {
-        ctx.texImage2D(...textureArgs);
-    } catch (err) {
-        throw err;
-    }
-
-    [
-        [ctx.TEXTURE_WRAP_S, ctx.CLAMP_TO_EDGE],
-        [ctx.TEXTURE_WRAP_T, ctx.CLAMP_TO_EDGE],
-        [ctx.TEXTURE_MIN_FILTER, ctx[filter]],
-        [ctx.TEXTURE_MAG_FILTER, ctx[filter]]
-    ].forEach(
-        ([a, b]) => { ctx.texParameteri(ctx.TEXTURE_2D, a, b) }
-    );
-    ctx.bindTexture(ctx.TEXTURE_2D, null);  // prevent accidental use
-
-    return texture
-};
-
 
 /**
- * Abstraction for binding array data into GPU memory
+ * Returns falsey value if there is no graphics context available,
+ * otherwise return the context handle.
  */
-export class ArrayBuffer {
-    constructor(ctx, data) {
-        this.buffer = ctx.createBuffer();
-        ctx.bindBuffer(ctx.ARRAY_BUFFER, this.buffer);
-        ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(data), ctx.STATIC_DRAW);
-    }
-};
+export const validContext = (ref, context="webgl") =>  
+    (!ref || !ref.current) ? false : ref.current.getContext(context);
+    
 
 
 /**
@@ -138,7 +70,7 @@ const CLOCK_UNIFORM = "u_time";
  * Execute a shader program and all binding steps needed to make data
  * available to the hardware
  */
-export const renderPipelineStage = ({
+const renderPipelineStage = ({
     runtime, 
     ctx, 
     uniforms, 
@@ -231,4 +163,104 @@ export const renderPipeline = (
 ) => {
     const args = {runtime, ctx, uniforms};
     pipeline.forEach(step => renderPipelineStage(args, step));
-}
+};
+
+
+
+export const extractUniforms = (keys, uniforms) => 
+    keys.map(k => [k, uniforms[k]]);
+
+
+
+export const createTexture = ({ 
+    ctx, 
+    filter = "NEAREST", 
+    data, 
+    shape = [null, null] 
+}) => {
+
+    let texture = ctx.createTexture();
+    const args = data instanceof Uint8Array ? [...shape, 0] : [];
+
+    ctx.bindTexture(ctx.TEXTURE_2D, texture);
+    const textureArgs = [ctx.TEXTURE_2D, 0, ctx.RGBA, ...args, ctx.RGBA, ctx.UNSIGNED_BYTE, data];
+
+    try {
+        ctx.texImage2D(...textureArgs);
+    } catch (err) {
+        throw err;
+    }
+
+    [
+        [ctx.TEXTURE_WRAP_S, ctx.CLAMP_TO_EDGE],
+        [ctx.TEXTURE_WRAP_T, ctx.CLAMP_TO_EDGE],
+        [ctx.TEXTURE_MIN_FILTER, ctx[filter]],
+        [ctx.TEXTURE_MAG_FILTER, ctx[filter]]
+    ].forEach(
+        ([a, b]) => { ctx.texParameteri(ctx.TEXTURE_2D, a, b) }
+    );
+    ctx.bindTexture(ctx.TEXTURE_2D, null);  // prevent accidental use
+
+    return texture
+};
+
+
+/**
+IFF the canvas context is WebGL and we need shaders, fetch the GLSL code and compile the 
+shaders as programs.
+
+This is executed only once, after the WASM runtime is loaded. 
+*/
+export const useGlslShaders = ({
+    ref, 
+    shaders,
+    callback=null
+}) => {
+   
+    const [programs, setPrograms] = useState(null);
+    const runtime = useWasmRuntime();
+
+    useEffect(() => {
+
+        if (!runtime || typeof ref === "undefined" || !ref.current) return;
+        const ctx = ref.current.getContext("webgl");
+        if (!ctx) return;
+
+        (async () => {
+            
+            const compiled = Object.fromEntries(await Promise.all(Object.entries(shaders).map(async ([programName, pair]) => {
+    
+                let [vs, fs] = pair.map(async (file) => {
+                    if (!(file in shaderSource)) shaderSource[file] = await runtime.fetch_text(`/${file}.glsl`)
+                    return shaderSource[file];
+                });
+                const program = runtime.create_program(ctx, await vs, await fs);
+                let wrapper = { program };
+
+                [
+                    ["ATTRIBUTES", "Attrib"], 
+                    ["UNIFORMS", "Uniform"]
+                ].forEach(
+                    ([key, fcn])=>{
+                        for (let ii = 0; ii < ctx.getProgramParameter(program, ctx[`ACTIVE_${key}`]); ii++) {
+                            const { name } = ctx[`getActive${fcn}`](program, ii);
+                            wrapper[name] = ctx[`get${fcn}Location`](program, name);
+                        }
+                    }
+                );
+
+                return [programName, wrapper];
+            })));
+    
+            setPrograms(compiled);
+        })();
+    }, [runtime, ref]);
+
+    return {
+        programs,
+        validContext: () => (!ref || !ref.current) ? false : ref.current.getContext("webgl")
+    }
+
+};
+
+export default useGlslShaders;
--- src/hooks/useLagrangianTest.jsx
+++ src/hooks/useLagrangianTest.jsx
@@ -1,24 +1,17 @@
 import { useEffect, useState, useRef } from "react";
-import { useGlslShaders, createTexture, ArrayBuffer, renderPipeline } from "./useGlslShaders";
+import { useGlslShaders, createTexture, VertexArrayBuffers, renderPipeline } from "./useGlslShaders";
 import useWasmRuntime from "./useWasmRuntime";
 import useCanvasColorRamp from "./useCanvasColorRamp";
 
-
-const VERTEX_ARRAY_BUFFER = "a_index";
-const QUAD_ARRAY_BUFFER = "a_pos";
+/**
+ * Mapping of uniforms to program components
+ */
 const parameters = {
     screen: ["u_screen", "u_opacity"],
     sim: ["speed",  "drop",  "bump", "seed", "u_wind_res"],
     wind: ["u_wind", "u_particles", "u_color_ramp", "u_particles_res", "u_wind_max", "u_wind_min"],
     color: ["u_color_ramp", "u_opacity"],
 };
-
-const shaders = {
-    draw: ["draw-vertex", "draw-fragment"],
-    screen: ["quad-vertex", "screen-fragment"],
-    update: ["quad-vertex", "update-fragment"]
-};
-
 
  /**
   * Use WebGL to calculate particle trajectories from velocity data. 
@@ -29,11 +22,10 @@ const shaders = {
 export default ({
     source,
     metadataFile,
-    preview=null,
-    showVelocityField=false,
-    res = Math.ceil(Math.sqrt(4000)),
+    res = 16,
     colors = {
         0.0: '#dd7700',
+        0.5: '#deabab',
         1.0: '#660066'
     },
     opacity = 0.98, // how fast the particle trails fade on each frame
@@ -42,12 +34,44 @@ export default ({
     bump = 0.01 // drop rate increase relative to individual particle speed 
 }) => {
    
+    /**
+     * Exported ref to assign to an HTML canvas component
+     */
     const ref = useRef(null);
-    const [ assets, setAssets ] = useState(null);
-    const { programs } = useGlslShaders({ref, shaders});
+
+    /**
+     * Exported ref to draw textures to a secondary HTML canvas component
+     */
+    const preview = useRef(null);
+
+    /**
+     * Shader programs compiled from GLSL source.
+     */
+    const { programs, validContext } = useGlslShaders({
+        ref, 
+        shaders: {
+            draw: ["draw-vertex", "draw-fragment"],
+            screen: ["quad-vertex", "screen-fragment"],
+            update: ["quad-vertex", "update-fragment"]
+        }
+    });
+
+    /**
+     * Rust-WASM binary and JavaScript bindings for numerical
+     * components
+     */
     const runtime = useWasmRuntime();
+
+    /**
+     * Paints a colormap to a hidden canvas and then samples it as 
+     * a lookup table for speed calculations.
+     */
     const colorMap = useCanvasColorRamp({colors});
 
+    /**
+     * Particle locations
+     */
+    const [ particles, setParticles ] = useState(null);
 
     /**
     * Create a random distribution of particle positions,
@@ -57,7 +81,6 @@ export default ({
     * effect so that additional logic can be applied, 
     * or initial positions can be loaded from a DB or file.
     */
-    const [particles, setParticles] = useState(null);
     useEffect(() => {
         setParticles(
             new Uint8Array(Array.from(
@@ -67,24 +90,60 @@ export default ({
         );
     }, []);
 
+    /**
+     * Interpreting image formatted velocity data requires having
+     * infomration about the range. 
+     */
+    const [ metadata, setMetadata ] = useState(null);
 
     /**
-    * Fetch the metadata file. 
-    * 
-    * This has no dependencies and will probably be executed first. 
-    * In the future this may support static files or DB queries. 
-    */
-    const [metadata, setMetadata] = useState(null);
+     * Fetch the metadata file. 
+     * 
+     * This has no dependencies and will probably be executed first. 
+     * In the future this may support static files or DB queries. 
+     */
     useEffect(() => {
+        if (!metadataFile) return;
         fetch(metadataFile)
             .then(r => r.json())
-            .then(meta => {setMetadata(meta)})
+            .then(setMetadata)
             .catch(err => {
                 console.log("Metadata Error", err)
             });
     }, []);
 
-    
+
+    /**
+     * Container for handles to GPU interface
+     */
+    const [ assets, setAssets ] = useState(null);
+
+
+    /**
+     * Use external data as a velocity field to force movement of particles
+     */
+    useEffect(()=>{
+        const ctx = validContext();
+        if (!ctx || !source || !particles || !colorMap) return;
+      
+        const img = new Image();
+        img.addEventListener('load', () => {
+            setAssets({
+                img,
+                textures: {
+                    uv: createTexture({ctx: ctx, filter: "LINEAR", data: img})
+                },
+                uniforms: []
+            });
+        }, {
+            capture: true,
+            once: true,
+        });
+        img.crossOrigin = source.includes(".") ? "" : undefined;
+        img.src = source;
+
+    },[]);
+
     /**
     * Generate assets and handles for rendering to canvas.
     * Use transducer to replace configs with texture instances.
@@ -93,20 +152,33 @@ export default ({
     * and the initial positions have been loaded or generated
     */
     useEffect(() => {
-        if (!ref || !ref.current || !particles || !metadata || !colorMap) return;
-        const ctx = ref.current.getContext("webgl");
-        if (!ctx) return;
-
+        const ctx = validContext();
+        if (!ctx || !particles || !metadata || !colorMap) return;
+    
         const { width, height } = ref.current;
         const shape = [ width, height ];
         const { u, v } = metadata;
         const size = width * height * 4;
+        const uniforms = {
+            "u_screen" : ["i", 2],
+            "u_opacity": ["f", opacity],
+            "u_wind": ["i", 0],
+            "u_particles": ["i", 1],
+            "u_color_ramp": ["i", 2],
+            "u_particles_res": ["f", res],
+            "u_wind_max": ["f", [u.max, v.max]],
+            "u_wind_min": ["f", [u.min, v.min]],
+            "speed": ["f", speed],
+            "drop": ["f", drop],
+            "bump": ["f", bump],
+            "seed": ["f", Math.random()],
+            "u_wind_res": ["f", [width, height]]
+        }
 
         const img = new Image();
         img.addEventListener('load', () => {
-
             setAssets({
-                image: img,
+                img,
                 textures: 
                     Object.fromEntries(Object.entries({
                         screen: { data: new Uint8Array(size), shape },
@@ -118,27 +190,12 @@ export default ({
                     }).map(
                         ([k, v]) => [k, createTexture({ctx: ctx, ...v})]
                     )),
-                buffers: {
-                    quad: [(new ArrayBuffer(ctx, [0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1])).buffer, QUAD_ARRAY_BUFFER, 2],
-                    index: [(new ArrayBuffer(ctx, particles)).buffer, VERTEX_ARRAY_BUFFER, 1]
-                },
+                buffers: VertexArrayBuffers(ctx, particles),
                 framebuffer: ctx.createFramebuffer(),
-                uniforms: {
-                    "u_screen" : ["i", 2],
-                    "u_opacity": ["f", opacity],
-                    "u_wind": ["i", 0],
-                    "u_particles": ["i", 1],
-                    "u_color_ramp": ["i", 2],
-                    "u_particles_res": ["f", res],
-                    "u_wind_max": ["f", [u.max, v.max]],
-                    "u_wind_min": ["f", [u.min, v.min]],
-                    "speed": ["f", speed],
-                    "drop": ["f", drop],
-                    "bump": ["f", bump],
-                    "seed": ["f", Math.random()],
-                    "u_wind_res": ["f", [width, height]]
-                }
+                uniforms
             });
+
+
         }, {
             capture: true,
             once: true,
@@ -148,12 +205,21 @@ export default ({
 
     }, [ref, particles, metadata, colorMap]);
 
-
+    /**
+     * Display the wind data in a secondary 2D HTML canvas, for debugging
+     * and interpretation. 
+     */
     useEffect(() => {
+        if (!preview || !preview.curent || !assets || !assets.img) return;
+        preview.current.getContext("2d").drawImage(assets.img, 0, 0, preview.current.width, preview.current.height);
+    }, [preview, assets]);
 
-        if (!programs || !ref.current || !assets || !metadata) return;
-        const ctx = ref.current.getContext("webgl");
-        if (!ctx) return;
+    /**
+     * Start the rendering loop
+     */
+    useEffect(() => {
+        const ctx = validContext();
+        if (!ctx || !programs || !assets || !metadata) return;
 
         let requestId;  
         let {
@@ -225,19 +291,8 @@ export default ({
         return () => cancelAnimationFrame(requestId);
     }, [programs, assets]);
 
-
     /**
-     * Display the wind data in a secondary 2D HTML canvas, for debugging
-     * and interpretation. 
+     * Resources available to parent Component or Hook.
      */
-    useEffect(() => {
-        if (!showVelocityField || !preview || !preview.current || !assets || !assets.image) return;
-
-        let ctx = preview.current.getContext('2d');
-        if (!ctx) return;
-
-        ctx.drawImage(assets.image, 0, 0, preview.current.width, preview.current.height);
-    }, [preview, assets]);
-
     return {ref}
 };
