61382dd34e94b2ec8fbb542c3cd3b24f128c048e
Nicholas Keeney <nkeeney@oceanics.io>
2021-05-11 06:35:41 -0400

    round two, consolidate and dedup

--- bathysphere/bathysphere/capsize.py
+++ bathysphere/bathysphere/capsize.py
@@ -49,18 +49,6 @@ OSI_DATASET = "bivalve-suitability"
 IndexedDB = dict()
 
 
-@pytest.fixture(scope="session")
-def client():
-    """
-    Connexion Apps are a wrapper around the real Flask App.
-
-    This yields the TestClient for making API calls with pytest.
-    """
-    app.app.config["DEBUG"] = True
-    app.app.config["TESTING"] = True
-    with app.app.test_client() as c:
-        yield c
-
 
 def points(n=10):
     return random.uniform(size=(n, 2))
--- bathysphere/bathysphere/models.py
+++ bathysphere/bathysphere/models.py
@@ -46,6 +46,101 @@ from capsize.utils import (
     normal,
     Path
 )
+# pylint: disable=invalid-name
+from numpy import zeros, arange
+from connexion import App
+from flask_cors import CORS
+from pathlib import Path
+from prance import ResolvingParser, ValidationError
+from os import getenv
+from requests import get
+
+
+class Memory:
+    def __init__(self, size, max_size=int(1e6)):
+        # type: (int, int) -> None
+        """
+        Memory manager class for allocating and freeing bytes string, only implements contiguous chunks.
+        """
+        if not isinstance(size, int):
+            raise TypeError
+        if size > max_size:
+            raise MemoryError
+
+        self.buffer = zeros(size, dtype=bytes)
+        self.mask = zeros(size, dtype=bool)
+        self.map = dict()
+        self.remaining = size
+        self._count = 0
+
+    def alloc(self, size):
+        # type: (int) -> int
+        """
+        Allocate and return a fixed length buffer. Raise error if out of memory.
+        """
+        if self.remaining < size:
+            raise MemoryError
+
+        # find indices of sufficient free memory, return pointers
+        # optionally shuffle memory to create contiguous blocks
+        self._count += 1
+        self.remaining -= size
+
+        start = self._find(size)
+        if start is None:
+            raise MemoryError
+
+        ptr = self.buffer[start : start + size]
+        self.map[self._count] = {"mask": arange(start, start + size), "data": ptr}
+        return self._count
+
+    def set(self, key, values):
+        # type: (int or str, bytes) -> None
+        """
+        Set buffer to specified values, or singleton
+        """
+        self.map[key]["data"][:] = values
+
+    def data(self, key):
+        # type: (int or str) -> bytes
+        """Return data"""
+        return self.map[key]["data"]
+
+    def free(self, key):
+        # type: (int or str) -> bool
+        """
+        Free previously allocated variable
+        """
+        try:
+            indices = self.map[key]["mask"]  # get indices from memory map dict
+            # reset mask and increment available memory
+            self.mask[indices] = False
+            self.remaining += len(indices)
+            del key
+
+        except (MemoryError, TypeError):
+            return False
+        else:
+            return True
+
+    def _find(self, size):
+        # type: (int) -> int or None
+        """Find the starting index of the first available contiguous chunk"""
+        start = 0
+        while True:
+            offset = 1
+            if not self.mask[start]:
+                while not self.mask[start + offset] and offset <= size:
+                    if offset == size:
+                        return start
+                    else:
+                        offset += 1
+            else:
+                start += 1
+
+            if start == len(self.mask) - size:
+                return None
+
 
 ExtentType = (float, float, float, float)
 
