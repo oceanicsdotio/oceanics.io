92f838d3b884a7c55c6f44eaf66544eb197a5e86
Nicholas Keeney <nkeeney@oceanics.io>
2021-05-24 09:19:06 -0400

    fancy greek probably inaccessible?

--- src/components/LayerCard.jsx
+++ src/components/LayerCard.jsx
@@ -1,48 +1,48 @@
 /**
  * React and friends.
  */
- import React, { useMemo } from "react";
+import React, { useMemo } from "react";
 
- /**
-  * Component-level styling.
-  */
- import styled from "styled-components";
- 
- /**
-  * Predefined colors.
-  */
- import { ghost, orange, grey } from "../palette";
- 
- 
- /**
-  * Span like div for indicating the zoom level at which the layer appears.
-  * 
-  * @param {*} param0 
-  * @returns 
-  */
- const ZoomIndicator = ({className, zoom}) => {
-     return <div className={className} zoom={zoom}>
-         {`zoom: ${zoom[0]}-${zoom[1]}`}
-     </div>
- };
- 
- /**
-  * Adjust margins and width to indicate the zoom level of the
-  */
- const StyledZoomIndicator = styled(ZoomIndicator)`
-    
-     height: auto;
-     border: 1px solid;
-     margin-bottom: 15px;
-     margin-left: ${({zoom})=>(zoom[0]-1)/22*100}%;
-     margin-right: ${({zoom})=>(22-zoom[1])/22*100}%;
-     color: ${({ zoom, zoomLevel}) => (zoomLevel === null || (zoomLevel >= zoom[0]) && (zoomLevel <= zoom[1])) ? orange : grey};
- `;
- 
- /**
-  * Emoji button, cuz it unicode baby. 
-  */
- const Emoji = styled.a`
+/**
+ * Component-level styling.
+ */
+import styled from "styled-components";
+
+/**
+ * Predefined colors.
+ */
+import { ghost, orange, grey } from "../palette";
+
+
+/**
+ * Span like div for indicating the zoom level at which the layer appears.
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
+const ZoomIndicator = ({className, zoom}) => {
+    return <div className={className} zoom={zoom}>
+        {`zoom: ${zoom[0]}-${zoom[1]}`}
+    </div>
+};
+
+/**
+ * Adjust margins and width to indicate the zoom level of the
+ */
+const StyledZoomIndicator = styled(ZoomIndicator)`
+
+    height: auto;
+    border: 1px solid;
+    margin-bottom: 15px;
+    margin-left: ${({zoom})=>(zoom[0]-1)/22*100}%;
+    margin-right: ${({zoom})=>(22-zoom[1])/22*100}%;
+    color: ${({ zoom, zoomLevel}) => (zoomLevel === null || (zoomLevel >= zoom[0]) && (zoomLevel <= zoom[1])) ? orange : grey};
+`;
+
+/**
+ * Emoji button, cuz it unicode baby. 
+ */
+const Emoji = styled.a`
     margin: 5px;
     padding: 2px;
     border-radius:50%;
@@ -50,7 +50,7 @@
     cursor: pointer;
     border: 1px dashed ${ghost};
     text-decoration: none !important;
- `;
+`;
 
  
 /**
