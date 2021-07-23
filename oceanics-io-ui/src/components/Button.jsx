9f66a414c0b95b14a27621b0e9bfc37ee94ddadd
Nicholas Keeney <nkeeney@oceanics.io>
2021-07-23 17:35:05 -0400

    new noe4j host, structure

--- oceanics-io-ui/src/components/Button.jsx
+++ oceanics-io-ui/src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {orange, grey, charcoal} from "./palette";
+import {orange, grey, charcoal} from "../palette";
 
 
 import {Input} from "./Input";
