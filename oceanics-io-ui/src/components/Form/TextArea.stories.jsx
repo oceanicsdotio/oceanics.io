8d944c018dfe200eab13a78aedbf7d2e69ca4635
Nicholas Keeney <nkeeney@oceanics.io>
2021-08-01 15:07:11 -0400

    TextArea stories

--- oceanics-io-ui/src/components/Form/TextArea.stories.jsx
+++ oceanics-io-ui/src/components/Form/TextArea.stories.jsx
@@ -1,16 +1,44 @@
+/**
+ * React and friends
+ */
 import React from 'react';
 
+/**
+ * Base component
+ */
 import TextArea from './TextArea';
 
+/**
+ * Global styles
+ */
+import "../../styles/global.css";
+import "../../styles/theme.css";
+
+/**
+ * Storybook config
+ */
 export default {
   component: TextArea,
   title: 'Form/TextArea',
 }
 
-//👇 We create a “template” of how args map to rendering
-const Template = ({}) => <TextArea>{"Some text"}</TextArea>;
+/**
+ * Template to build cases from
+ */
+const Template = ({children}) => <TextArea>{children}</TextArea>;
 
-//👇 Each story then reuses that template
-export const Default = Template.bind({});
+/**
+ * Case with a short string
+ */
+export const Short = Template.bind({});
+Short.args = {
+    children: "Some text"
+};
 
-Default.args = {};
+/**
+ * Case with a long string
+ */
+export const Long = Template.bind({});
+Long.args = {
+    children: "some text ".repeat(127)
+};
