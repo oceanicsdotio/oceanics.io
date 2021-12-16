1767b78763634eac770a4ae9141bc49c27faa14c
Keeney <nkeeney@oceanics.io>
2021-12-16 05:54:44 -0500

    wharfinger post

---
+++ test/test-api.js
@@ -0,0 +1,28 @@
+
+var assert = require('assert');
+
+describe('Auth API', function() {
+  describe('Register', function() {
+    it('should allowed registration with API key', function() {
+      
+    });
+    it('should prevent registration without API key', function() {
+      
+    });
+    it('should prevent registration with wrong API key', function() {
+      
+    });
+  });
+
+  describe('Get JWT', function() {
+    it('should return well-formed token given credentials', function() {
+      
+    });
+    it('should deny access without credentials', function() {
+      
+    });
+    it('should deny access with wrong credentials', function() {
+      
+    });
+  });
+});
