bdd4b2f917931ae6a7f6bcd92660ce9103be2923
Keeney <nkeeney@oceanics.io>
2022-09-01 19:28:08 -0400

    test script

--- test-cache.js
+++ test-cache.js
@@ -12,12 +12,14 @@ import matter from "gray-matter";
 import path from "path";
 import {parseAllDocuments} from "yaml";
 
-// Commandline args
+// Command-line args
 const [
   API_SPECIFICATION,
   CACHE
 ] = process.argv.slice(2)
 
+const UUID = "uuid";
+const ENCODING = "utf8";
 const ASSETS = "./oceanics-io-www/public/assets";
 const ICON_METADATA = `${ASSETS}/oceanside.yml`;
 const FORMAT = ".mdx";
@@ -26,46 +28,40 @@ const REFERENCES = path.join(process.cwd(), "./oceanics-io-content");
 // Utility functions for chaining
 const filterPng = (name) => name.endsWith(".png");
 const wrapSlug = (slug) => Object({ slug });
+const wrapMeta = (metadata) => Object({metadata});
 const filterMdx = (name) => name.endsWith(FORMAT);
 const getSlug = (name) => Object({ params: { slug: name.split(".").shift() } });
 
 // Read content of a resource and parse it
 const readDocument = async ({ params: {slug}}) => {
   const file = path.join(REFERENCES, `${slug}${FORMAT}`);
-  return fs.readFile(file, {encoding: "utf8"});
-};
-
-// Convert from text to structured data
-const parseDocument = (slug) => (text) => {
+  const text = await fs.readFile(file, ENCODING);
   const { data: {references=[], ...metadata}, content } = matter(text);
+  metadata.references = references.map(wrapMeta);
   return {
-      metadata: {
-          ...metadata,
-          references: references.map((metadata) => Object({metadata}))
-      },
+      metadata,
       content,
       slug
   }
-}
+};
 
-// Strip navigation props from instance
-const filterNavigation = ([key]) => !key.includes("@");
-
-// Unpack UUI and de-normalize
-const flattenNode = (label) => {
-  return (props) => {
+// OpenAPI schema to flat list of examples
+const schemaToLookup = ([label, { examples = [] }]) => {
+  // Strip navigation props from instance
+  const _filterNavigation = ([key]) => !key.includes("@"); 
+  // Unpack UUI and de-normalize
+  const _flattenNode = (props) => {
     const uuid = crypto.randomUUID();
-    const filtered = Object.entries(props).filter(filterNavigation);
+    const filtered = Object.entries(props).filter(_filterNavigation);
     return [
       label,
       uuid,
-      Object.fromEntries([...filtered, ["uuid", uuid]])
+      Object.fromEntries([...filtered, [UUID, uuid]])
     ]
   }
+  return examples.map(_flattenNode);
 }
-// OpenAPI schema to flat list of examples
-const schemaToLookup = ([label, { examples = [] }]) => 
-  examples.map(flattenNode(label));
+  
 
 // Concurrently load all of the idempotent data for processing
 const [
@@ -74,27 +70,33 @@ const [
   _assets,
   _text
 ] = await Promise.all([
-  fs.readFile(path.join(process.cwd(), ICON_METADATA), "utf8"),
+  fs.readFile(path.join(process.cwd(), ICON_METADATA), ENCODING),
   fs.readdir(REFERENCES),
   fs.readdir(path.join(process.cwd(), ASSETS)),
-  fs.readFile(API_SPECIFICATION, "utf-8")
+  fs.readFile(API_SPECIFICATION, ENCODING)
 ]);
 
 // Concurrently load all of the MDX files
 const index = _references.filter(filterMdx).map(getSlug);
 const documents = await Promise.all(index.map(readDocument));
 
+// Parse schema examples
+const examples = Object.entries(JSON.parse(_text).components.schemas);
+const data = examples.flatMap(schemaToLookup);
+
 // Structure cache data
-const data = {
-  nodes: (Object.entries(JSON.parse(_text).components.schemas)).flatMap(schemaToLookup),
-  index,
-  documents: documents.map(parseDocument),
-  icons: {
-      sources: _assets.filter(filterPng).map(wrapSlug),
-      templates: parseAllDocuments(_icons).map((doc) => doc.toJSON())
-  }
-}
+// const data = {
+//   nodes: examples.flatMap(schemaToLookup),
+//   index,
+//   documents,
+//   icons: {
+//       sources: _assets.filter(filterPng).map(wrapSlug),
+//       templates: parseAllDocuments(_icons).map((doc) => doc.toJSON())
+//   }
+// }
 
 // Write out the cache
 console.warn(`writing new cache: ${CACHE}`);
 await fs.writeFile(CACHE, JSON.stringify(data));
+
+export {}
