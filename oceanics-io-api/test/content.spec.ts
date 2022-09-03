5210a384d7e7dd51b11db4ab2aeab38488b712c4
Keeney <nkeeney@oceanics.io>
2022-09-03 14:52:33 -0400

    move tests, to allow esbuild direct

--- oceanics-io-api/test/content.spec.ts
+++ oceanics-io-api/test/content.spec.ts
@@ -2,7 +2,7 @@
  * Endpoint allows bulk upload of content metadata. 
  */
 import { describe, expect, test } from '@jest/globals';
-import { getContent } from './shared/test-utils';
+import { getContent } from './test-utils';
 import fetch from "node-fetch";
 
 const url = "http://localhost:8888/.netlify/functions/content"
--- oceanics-io-api/test/storage.spec.ts
+++ oceanics-io-api/test/storage.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { apiFetch } from "./shared/test-utils";
+import { apiFetch } from "./test-utils";
 
 export const GEOMETRY = "https://gis.maine.gov/arcgis/rest/services/Boundaries/Maine_Boundaries_Town_Townships/MapServer/2/query?where=LAND%20%3D%20%27n%27&outFields=OBJECTID,TOWN,COUNTY,ISLAND,ISLANDID,TYPE,Shape,GlobalID,Shape.STArea(),last_edited_date,CNTYCODE&outSR=4326&f=json"
 
