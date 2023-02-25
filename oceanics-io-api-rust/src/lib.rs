722d645ea41e4e32ad961cf6f34555c40b8d6a91
Keeney <nkeeney@oceanics.io>
2023-02-25 11:35:55 -0500

    idk

--- oceanics-io-api-rust/src/lib.rs
+++ oceanics-io-api-rust/src/lib.rs
@@ -7,6 +7,11 @@ use wasm_bindgen::prelude::*;
 
 extern crate console_error_panic_hook;
 
+/**
+ * Bubble up error messages to JavaScript runtime
+ * in a more consistent way. Needs to be run once
+ * after the WASM module is loaded. 
+ */
 #[wasm_bindgen{js_name = "panicHook"}]
 pub fn panic_hook() {
     console_error_panic_hook::set_once();
