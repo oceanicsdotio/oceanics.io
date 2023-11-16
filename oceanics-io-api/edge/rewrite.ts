b8ad07e5653adb41a8b7c61bf3fe41e556b10e22
Keeney <nkeeney@oceanics.io>
2023-11-16 16:41:56 -0500

    API updates

--- oceanics-io-api/edge/rewrite.ts
+++ oceanics-io-api/edge/rewrite.ts
@@ -1,9 +1,5 @@
-// @ts-ignore: tsc doesn't get deno?
-import type { Context } from "https://edge.netlify.com";
-
 const AUTH = "auth";
 const lookup = {
-  [AUTH]: AUTH,
   0: `index`,
   1: `collection`,
   2: `entity`,
@@ -33,7 +29,7 @@ const routeFromUrl = (url: URL) =>
     .filter(filterPath)
 
 // Replace parenthesis syntax with paths, because it's WAY easier
-export default (request: Request, context: Context) => {
+export default (request: Request) => {
   const hasAuth = request.headers.has("x-api-key") || request.headers.has("authorization")
   if (!hasAuth) return new Response("Unauthorized", {
     status: 403
@@ -41,11 +37,10 @@ export default (request: Request, context: Context) => {
   
   const url = new URL(request.url)
   const route = routeFromUrl(url);
-  const count = route.length as keyof typeof lookup;
-  if (count > 0 && route[0] === AUTH) {
-    return context.rewrite(`/.netlify/functions/auth`)
+  if (route.length > 0 && route[0] === AUTH) {
+    return new URL(`/.netlify/functions/auth`, request.url)
   }
-  const endpoint: string = lookup[count] ?? "";
+  const endpoint: string = lookup[route.length as 0|1|2|3] ?? "";
   if (!endpoint) {
     return new Response("Not Found", {
       status: 404
@@ -54,7 +49,7 @@ export default (request: Request, context: Context) => {
   const search = new URLSearchParams();
   const [left="", uuid="", right=""] = route;
 
-  if (count > 0) {
+  if (route.length > 0) {
     if (!SENSING.has(left)) {
       return new Response("Not Found", {
         status: 404
@@ -62,8 +57,8 @@ export default (request: Request, context: Context) => {
     }
     search.append('left', left);
   }
-  if (count > 1) search.append('uuid', uuid);
-  if (count > 2) {
+  if (route.length > 1) search.append('uuid', uuid);
+  if (route.length > 2) {
     if (!SENSING.has(right)) {
       return new Response("Not Found", {
         status: 404
