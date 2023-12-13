fcde623f1d06b7e493b335778a4359710a58f6bc
Keeney <nkeeney@oceanics.io>
2023-12-13 17:26:31 -0500

    index ease in load

--- oceanics-io-www/src/components/Oceanside/Oceanside.tsx
+++ oceanics-io-www/src/components/Oceanside/Oceanside.tsx
@@ -38,12 +38,32 @@ export const Oceanside = ({ className, ...props }: ApplicationType) => {
   );
 };
 
+const CanvasPlaceholder = ({className}: {className?: string}) => {
+  return <div className={className}></div>
+}
+export const StyledCanvasPlaceholder = styled(CanvasPlaceholder)`
+  display: block;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  aspect-ratio: 1 / 0.8;
+`;
+
 export const StyledViewport = styled(Oceanside)`
 
   display: block;
   margin: 0;
   padding: 0;
   width: 100%;
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 100%;
+    }
+  }
 
   & * {
     margin: 0;
@@ -61,6 +81,7 @@ export const StyledViewport = styled(Oceanside)`
   & .${BOARD} {
     width: 100%;
     aspect-ratio: 1 / 0.8;
+    animation: 1s ease-out 0s 1 fade-in;
   }
 `;
 
