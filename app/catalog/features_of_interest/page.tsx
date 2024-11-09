14edbfcde015483ddbd35d7c1f05ad204ffcdff4
Keeney <nkeeney@oceanics.io>
2024-11-09 14:10:15 -0500

    syntax only

--- app/catalog/features_of_interest/page.tsx
+++ app/catalog/features_of_interest/page.tsx
@@ -4,21 +4,15 @@ import Client from "@app/catalog/features_of_interest/client";
 import { CollectionTemplate, formatMetadata } from "@catalog/page";
 import type { Metadata } from "next";
 const schema = openapi.components.schemas.FeaturesOfInterest;
-/**
- * Page browser metadata
- */
 export const metadata: Metadata = formatMetadata("Read", schema);
 /**
  * Display an index of all or some subset of the
  * available Features of Interest in the database.
  */
 export default function Page({}) {
-  /**
-   * Static Component
-   */
   return (
     <CollectionTemplate schema={schema}>
-      <Client></Client>
+      <Client/>
     </CollectionTemplate>
   );
 }
